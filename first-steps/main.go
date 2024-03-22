d9d6c6cca11543d510a057484258ef289b681012
JustFiesta <mateusz.bocak9@gmail.com>
2024-03-22 09:30:53 +0100

    variable declaraion explanation

--- first-steps/main.go
+++ first-steps/main.go
@@ -4,4 +4,10 @@ import "fmt"
 
 func main()  {
 	fmt.Printf("Hello!")
+
+	// two types of variable declaration
+	x := 2
+	var y float32 = 0.332
+
+	fmt.Printf("You have to use delcared variables! %v, %v", x, y)
 }
